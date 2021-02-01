63b25361fa5b14021376c08c16fdd44db455300d
czjcxyly <78347486+czjcxyly@users.noreply.github.com>
2021-02-01 20:34:32 +0800

    Homework1

--- HW1.py
+++ HW1.py
@@ -1,10 +1,12 @@
-from pandas import Series, DataFrame
-data = {'Chinese': [68, 95, 98, 90,80], 'Math': [65, 76, 86, 88, 90], 'English': [30, 98, 88, 88, 90]}
-df1 = DataFrame(data, index=['ZhangFei', 'GuanYu', 'LiuBei', 'DianWei', 'XuChu'], columns=['Chinese', 'Math', 'English'])
-print(df1)
-print("语文,平均成绩：",df1['Chinese'].mean(),"，最低成绩：",df1['Chinese'].min(),"，最高成绩：",df1['Chinese'].max(),"，方差：",df1['Chinese'].var(),"，标准差：",df1['Chinese'].std(),"。")
-print("数学,平均成绩：",df1['Math'].mean(),"，最低成绩：",df1['Math'].min(),"，最高成绩：",df1['Math'].max(),"，方差：",df1['Math'].var(),"，标准差：",df1['Math'].std(),"。")
-print("英语,平均成绩：",df1['English'].mean(),"，最低成绩：",df1['English'].min(),"，最高成绩：",df1['English'].max(),"，方差：",df1['English'].var(),"，标准差：",df1['English'].std(),"。")
-df1['Sum']=df1.apply(lambda x:x.sum(),axis=1)
-print("总成绩排名：")
-print(df1.sort_values(by='Sum', ascending=False))
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+sum1=0
+b=[i for i in range(1,101)]
+for i in b[1::2]:
+    sum1+=i
+print("偶数和为：{}".format(sum1))
+
